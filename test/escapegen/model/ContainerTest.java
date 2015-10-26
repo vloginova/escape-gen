c13037f62cc1849533ec9f61324f5bcc633ebab8
Vita Loginova <vita.loginova@gmail.com>
2015-10-26 18:59:33 +0300

    test: Prepare build and test environment
    Add Ant build.xml, junit and hamcrest-core lib files and simple
    unit test.

---
+++ test/escapegen/model/ContainerTest.java
@@ -0,0 +1,25 @@
+package escapegen.model;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author - Vita Loginova
+ */
+public class ContainerTest {
+
+    @Test
+    public void testTryOpen() throws Exception {
+        Container container = new Container("TestContainer", Item.Size.Small) {
+            @Override
+            protected void showContent() {
+                /* do nothing */
+            }
+        };
+
+        assertTrue(container.tryOpen(new LinkedList<>()));
+    }
+}
