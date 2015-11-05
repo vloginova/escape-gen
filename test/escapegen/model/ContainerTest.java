9cee5cce77954c5e082c79ecc525a7173976d6bd
Vita Loginova <vita.loginova@gmail.com>
2015-11-05 16:33:52 +0300

    test: Add tests for Container class

--- test/escapegen/model/ContainerTest.java
+++ test/escapegen/model/ContainerTest.java
@@ -2,12 +2,32 @@ package escapegen.model;
 
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
  * @author - Vita Loginova
  */
 public class ContainerTest {
+    private class TestingContainer extends Container {
+
+        protected TestingContainer(String id) {
+            super(id, Size.Medium);
+            lock = new Lock() {
+                @Override
+                protected boolean unlock(Tool tool) {
+                    return true;
+                }
+            };
+            lock.addTool(new Tool(id + "Tool", Size.Small) { });
+        }
+
+        @Override
+        public void showContent() { }
+    }
 
     @Test
     public void testTryOpen() throws Exception {
@@ -19,5 +39,66 @@ public class ContainerTest {
         };
 
         assertTrue(container.tryOpen(null));
+    }
+
+    @Test
+    public void testDependsOn() throws Exception {
+        Container c1 = new TestingContainer("1");
+        List<Tool> tools1 = new LinkedList<>(c1.getLockTools());
+        Container c2 = new TestingContainer("2");
+        List<Tool> tools2 = new LinkedList<>(c2.getLockTools());
+        Container c3 = new TestingContainer("3");
+        List<Tool> tools3 = new LinkedList<>(c3.getLockTools());
+
+        c1.addDependencies(c2);
+        c1.addDependencies(c3);
+
+        assertTrue(c1.dependsOn(tools2.get(0)));
+        assertTrue(c1.dependsOn(tools3.get(0)));
+        assertTrue(c1.dependsOn(c2));
+        assertTrue(c1.dependsOn(c3));
+
+        assertFalse(c2.dependsOn(tools1.get(0)));
+        assertFalse(c2.dependsOn(c1));
+
+        assertFalse(c3.dependsOn(tools1.get(0)));
+        assertFalse(c3.dependsOn(c1));
+    }
+
+    @Test
+    public void testDependsOnTransitive() throws Exception {
+        Container c1 = new TestingContainer("1");
+        List<Tool> tools1 = new LinkedList<>(c1.getLockTools());
+        Container c2 = new TestingContainer("2");
+        List<Tool> tools2 = new LinkedList<>(c2.getLockTools());
+        Container c3 = new TestingContainer("3");
+        List<Tool> tools3 = new LinkedList<>(c3.getLockTools());
+
+        c1.addDependencies(c2);
+        c2.addDependencies(c3);
+
+        assertTrue(c1.dependsOn(tools2.get(0)));
+        assertTrue(c1.dependsOn(tools3.get(0)));
+        assertTrue(c1.dependsOn(c2));
+        assertTrue(c1.dependsOn(c3));
+
+        assertFalse(c2.dependsOn(tools1.get(0)));
+        assertFalse(c2.dependsOn(c1));
+        assertTrue(c2.dependsOn(tools3.get(0)));
+        assertTrue(c2.dependsOn(c3));
+
+        assertFalse(c3.dependsOn(tools1.get(0)));
+        assertFalse(c3.dependsOn(c1));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testDependsOnCircular() throws Exception {
+        Container c1 = new TestingContainer("1");
+        Container c2 = new TestingContainer("2");
+        Container c3 = new TestingContainer("3");
+
+        c2.addDependencies(c3);
+        c1.addDependencies(c2);
+        c3.addDependencies(c1);
     }
 }
