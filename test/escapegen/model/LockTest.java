20d029ae3d9dd5b02e02ebcbbbc7dd884b91a518
Vita Loginova <vita.loginova@gmail.com>
2015-11-05 14:35:34 +0300

    test: Add tests for Lock class

---
+++ test/escapegen/model/LockTest.java
@@ -0,0 +1,40 @@
+package escapegen.model;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author - Vita Loginova
+ */
+public class LockTest {
+
+    private class TestingLock extends Lock {
+        int count = 0;
+
+        public TestingLock(String name) {
+            tools.put(name, new Tool(name, Item.Size.Small) {});
+        }
+
+        @Override
+        protected boolean unlock(Tool tool) {
+            count++;
+            return tools.containsValue(tool);
+        }
+    }
+
+    @Test
+    public void testTryUnlock() throws Exception {
+        TestingLock lock = new TestingLock("Key");
+        List<Tool> tools = new LinkedList<>(lock.getTools());
+
+        assertFalse(lock.tryUnlock(null));
+        assertTrue(lock.tryUnlock(tools.get(0)));
+
+        assertTrue(lock.tryUnlock(null));
+        assertEquals(2, lock.count);
+    }
+}
